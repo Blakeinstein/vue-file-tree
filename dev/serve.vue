2497cbf41d36d134d7c6d35b8766de90899488e3
Blaine <rishi1998@gmail.com>
2021-09-10 15:16:36 +0530

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VueFileTree from '@/vue-file-tree.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueFileTree
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-file-tree />
+  </div>
+</template>
